f09c4b0929d19edf176f870d18ed073fc2fc78ef
nani <chotima.nani@gmail.com>
2024-01-30 20:00:34 +0700

    lab15_4.cpp completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &,int &,int &);
+void shuffle(int *,int *,int *,int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,9 +11,24 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
+}
+
+void shuffle(int *a,int *b,int *c,int *d){
+	int A[] = {*a,*b,*c,*d};
+	for(int i = 0; i < 3;i++){
+		int x = rand()%4;
+		int y = A[i];
+		A[i] = A[x];
+		A[x] = y;
+	}
+	*a = A[0];
+	*b = A[1];
+	*c = A[2];
+	*d = A[3];
+
 }
